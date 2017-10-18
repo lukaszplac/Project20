3131536948db1ab2f4c7259ea7a6affa69427839
lukaszplac <lukasz.plac@gmail.com>
2017-10-18 23:15:32 +0200

    Submodule taks 20.3 done. Ready to submit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store/store';
 import registerServiceWorker from './registerServiceWorker';
-import App from './App';
+import DevTools from './containers/DevTools';
+import * as actions from './actions/actions-countries'
 
+ReactDOM.render(
+  <Provider store={store}>
+    <div>
+      <DevTools/>
+    </div>
+  </Provider>
+  , document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+store.dispatch(actions.getCountries());
 registerServiceWorker();
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createStore } from 'redux';
+import reducers from '../reducers/index';
+import DevTools from '../containers/DevTools';
+
+const store = createStore(reducers, DevTools.instrument());
+
+export default store;
