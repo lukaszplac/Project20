05446372e32425a826052a0ff316227b74443df0
lukaszplac <lukasz.plac@gmail.com>
2017-11-01 19:33:09 +0100

    20_5 subtask done

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import routes from './routes';
+import DevTools from './containers/DevTools';
+import './country.css';
 
 render(
     <Provider store={store}>
+      <div>
         <Router history={hashHistory} routes={routes}/>
+        <DevTools />
+      </div>
     </Provider>,
     document.getElementById('root')
 );
